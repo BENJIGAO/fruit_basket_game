9ec229d2327ad31e984779e4dbd1f74ce10d8b47
Brian Gao <bhgao20014@gmail.com>
2021-12-02 12:08:49 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 // compile with: clang++ -std=c++20 -Wall -Werror -Wextra -Wpedantic -g3 -o fruits fruits.cpp
-// run with: ./fruits 2> /dev/null or ./fruits 2> bug.txt to not get weird messages in the terminal
+// run with: ./fruits 2> /dev/null or ./fruits 2> [name of .txt file of your choice] to avoid disruptive error messages
 //  "2>" redirect standard error (STDERR; cerr)
 //  /dev/null is a "virtual file" which discard contents
 
 // Works best in Visual Studio Code if you set:
 //   Settings -> Features -> Terminal -> Local Echo Latency Threshold = -1
-
-// https://en.wikipedia.org/wiki/ANSI_escape_code#3-bit_and_4-bit 
 
 #include <iostream>
 #include <vector>
@@ -28,8 +26,8 @@ using namespace std;
 #pragma clang diagnostic ignored "-Wunused-const-variable"
 
 const char NULL_CHAR     { 'z' };
-const char LEFT_CHAR     { 'a' };
-const char RIGHT_CHAR    { 'd' };
+const char LEFT_CHAR     { 'a' }; // won't work if caps lock is on
+const char RIGHT_CHAR    { 'd' }; // ^^^
 const char QUIT_CHAR     { 'q' };
 
 const string ANSI_START { "\033[" };
@@ -38,13 +36,8 @@ const string START_COLOUR_SUFFIX {"m"};
 const string STOP_COLOUR  {"\033[0m"};
 
 const unsigned int COLOUR_IGNORE  { 0 }; // this is a little dangerous but should work out OK
-const unsigned int COLOUR_BLACK   { 30 };
 const unsigned int COLOUR_RED     { 31 };
 const unsigned int COLOUR_GREEN   { 32 };
-const unsigned int COLOUR_YELLOW  { 33 };
-const unsigned int COLOUR_BLUE    { 34 };
-const unsigned int COLOUR_MAGENTA { 35 };
-const unsigned int COLOUR_CYAN    { 36 };
 const unsigned int COLOUR_WHITE   { 37 };
 
 const int START_ROW        { 1 };
@@ -52,13 +45,6 @@ const int END_ROW          { 16 };
 const int START_COLUMN     { 1 };
 const int MIDDLE_COLUMN    { 17 };
 const int END_COLUMN       { 37 };
-
-const unsigned short MOVING_NOWHERE { 0 };
-const unsigned short MOVING_LEFT    { 1 };
-const unsigned short MOVING_RIGHT   { 2 };
-const unsigned short MOVING_UP      { 3 };
-const unsigned short MOVING_DOWN    { 4 };
-
 
 #pragma clang diagnostic pop
 
@@ -73,27 +59,27 @@ struct termios initialTerm;
 // Types
 
 // Using signed and not unsigned to avoid having to check for ( 0 - 1 ) being very large
+
+
 struct position { int row; int col; };
 
 struct score
 {
     int score = 0;
-    unsigned int colour = COLOUR_GREEN;
+    unsigned int colour = COLOUR_WHITE;
 };
 
-struct fruit 
+struct fruit // one was added to the START_ROW and the value one was subtracted  from END-ROW to prevent conflict with the border 
 {
-    position position {START_ROW + 1, (lane( generator ) * 5) - 1 };
-    unsigned int colour = COLOUR_BLUE;
+    position position {START_ROW + 1, (lane( generator ) * 5) - 1 }; //the value is multiplied by 5 because the lane is 5 characters wide and subtract 1  to center the fruit and the lane 
+    unsigned int colour = COLOUR_GREEN;
 };
 
 struct basket
 {
-  position position { END_ROW - 1, MIDDLE_COLUMN }; // each lane the fruit falls is 3 columns wide
-  unsigned int colour = COLOUR_MAGENTA;
+  position position { END_ROW - 1, MIDDLE_COLUMN }; // each lane the fruit falls is 5 columns wide and 
+  unsigned int colour = COLOUR_WHITE;
 };
-
-typedef vector< fruit > fruitvector;
 
 // Utilty Functions
 
@@ -181,13 +167,13 @@ auto MakeColour( string inputString,
     return outputString;
 }
 
-// Fish Logic
+// Game Logic
+
 
 auto UpdateFruitPosition( fruit & fruit ) -> void
 {
     int rowChange = 1;
-
-    // Update the position of the fruit
+    // Ampersand were used as a reference so that the actual position updates
     fruit.position.row += rowChange;
 }
 
@@ -199,47 +185,47 @@ auto UpdateBasketPosition( basket & basket, char currentChar ) -> void
     if ( currentChar == LEFT_CHAR )  { commandColChange -= 5; }
     if ( currentChar == RIGHT_CHAR ) { commandColChange += 5; }
 
-    // Update the position of each fish
-    // Use a reference so that the actual position updates
+
     auto proposedCol { basket.position.col + commandColChange };
-    basket.position.col = max( START_COLUMN + 1, min( END_COLUMN - 5, proposedCol ) );
+    basket.position.col = max( START_COLUMN + 1, min( END_COLUMN - 5, proposedCol ) ); // 5 was subtracted from END-COLUMN to ensure the right side does not go past the border
 }
 
+/* Can be used if you want to spawn multiple fruits at a time
+
+typedef vector< fruit > fruitvector;
 auto CreateFruit( fruitvector & fruits ) -> void
 {
     cerr << "Creating Fruit" << endl;
     fruit newFruit { 
         .position = { .row = START_ROW + 1, .col = (( lane( generator ) ) * 5) - 1 } ,
-        .colour = COLOUR_MAGENTA,
+        .colour = COLOUR_GREEN,
     };
     fruits.push_back( newFruit );
-}
+} */
 
-auto DrawFruit(fruit fruit) -> void
+auto DrawFruit( fruit fruit ) -> void
 {
     MoveTo( fruit.position.row, fruit.position.col ); 
     cout << MakeColour("O", COLOUR_GREEN) << flush;
 }
 
-auto DrawBasket(basket basket ) -> void
+auto DrawBasket( basket basket ) -> void
 {
     MoveTo( basket.position.row, basket.position.col ); 
     cout << "\\___/" << flush; 
 }
 
-// Functions to do
-
-auto CheckInteraction(fruit fruit, basket basket ) -> bool
+// the IsCaught function is called when the fruit reaches the bottom row
+auto IsCaught( fruit fruit, basket basket ) -> bool
 {
-    if (fruit.position.col -2 == basket.position.col)
-
+    if (fruit.position.col - 2 == basket.position.col) // fruit is on the middle line, while the basket is on the left line, therefore two is subtracted to aallow it to align
     {
-        return true;
+    	return true;
     }
-        return false;
+    return false;
 }
 
-auto ResetFruit(fruit & fruit) -> void
+auto ResetFruit( fruit & fruit ) -> void
 {
     int newCol {(lane(generator) * 5) - 1};
     fruit.position.col = newCol;
@@ -248,14 +234,18 @@ auto ResetFruit(fruit & fruit) -> void
 
 auto DrawBoard() -> void
 {
-    for ( unsigned int row = START_ROW; row <= END_ROW; row++ ) {
-        if ( row == START_ROW || row == END_ROW ) {
-            for ( unsigned int column = START_COLUMN; column <= END_COLUMN; column++ ) {
+    for ( unsigned int row = START_ROW; row <= END_ROW; row++ ) 
+    {
+        if ( row == START_ROW || row == END_ROW ) 
+        {
+            for ( unsigned int column = START_COLUMN; column <= END_COLUMN; column++ ) 
+            {
                 MoveTo( row, column ); 
                 cout << "-" << flush;
             }
         }
-        else {
+        else 
+        {
             MoveTo( row, START_COLUMN ); 
             cout << "|" << flush;
             MoveTo( row, END_COLUMN ); 
@@ -264,26 +254,27 @@ auto DrawBoard() -> void
     }
 }
 
-auto DrawScore(score score) -> void
+auto DrawScore( score score ) -> void
 {
-    MoveTo(START_ROW, END_COLUMN + 2);
+    MoveTo(START_ROW, END_COLUMN + 2); // END_COLUMN + 2 to ensure the score is displayed outside the border
     cout << "score: " << to_string(score.score) << flush;
 }
 
-auto DrawGameOver(score score) -> void
+auto DrawGameOver( score score ) -> void
 {
+    // the values 8 and 14 were chosen for the "GAME OVER :(" message to ensure it is aligned in the middle of the box. The value 16 was chosen for score because it is shorter word
     MoveTo(8,14);
     cout << MakeColour("GAME OVER :(", COLOUR_RED) << flush;
     MoveTo(9,16);
     cout << "Score: " << to_string(score.score) << flush;
-    MoveTo(END_ROW, START_COLUMN);
+    MoveTo(END_ROW, START_COLUMN); // move command line out of the center
 }
 
-auto UpdateScore(score & score) -> void
+auto UpdateScore( score & score ) -> void
 {
     score.score++;
 }
-// drawBoard
+
 
 auto main() -> int
 {
@@ -292,28 +283,27 @@ auto main() -> int
 
     // Check that the terminal size is large enough for our fishies
     const position TERMINAL_SIZE { GetTerminalSize() };
-    if ( ( TERMINAL_SIZE.row < 20 ) or ( TERMINAL_SIZE.col < 38 ) )
+    if ( ( TERMINAL_SIZE.row < END_ROW ) or ( TERMINAL_SIZE.col < END_COLUMN + 11 ) ) // END_COLUMN + 11 to ensure enough space for the score
     {
         ShowCursor();
         TeardownScreenAndInput();
-        cout << endl <<  "Terminal window must be at least 20 by 22 to run this game" << endl;
+        cout << endl <<  "Terminal window must be at least " << to_string(END_ROW) << " by " << to_string(END_COLUMN + 11) <<  " to run this game" << endl;
         return EXIT_FAILURE;
     }
 
     // State Variables
-    fruitvector fruits;
     fruit fruit;
     basket basket;
     score score;
-    unsigned int ticks {0};
+    unsigned int ticks { 0 };
 
-    char currentChar { NULL_CHAR };
+    char currentChar { NULL_CHAR }; // do nothing until user presses left or right
 
     bool allowBackgroundProcessing { true };
 
     auto startTimestamp { chrono::steady_clock::now() };
     auto endTimestamp { startTimestamp };
-    int elapsedTimePerTick { 100 }; // Every 0.1s check on things
+    int elapsedTimePerTick { 100 }; // Every 0.1s check on things; used as the speed of the falling fruit
     
     SetNonblockingReadState( allowBackgroundProcessing );
     ClearScreen();
@@ -325,92 +315,62 @@ auto main() -> int
     {
         endTimestamp = chrono::steady_clock::now();
         auto elapsed { chrono::duration_cast<chrono::milliseconds>( endTimestamp - startTimestamp ).count() };
-        // We want to process input and update the world when EITHER  
-        // (a) there is background processing and enough time has elapsed
-        // (b) when we are not allowing background processing.
+        // We want to process input and update the world when enough time has elapsed
         if ( elapsed >= elapsedTimePerTick )
         {
             ticks++;
-            //cerr << "Ticks [" << ticks << "] allowBackgroundProcessing ["<< allowBackgroundProcessing << "] elapsed [" << elapsed << "] currentChar [" << currentChar << "]" << endl;
-            if ( currentChar == LEFT_CHAR || currentChar == RIGHT_CHAR ) {
-                UpdateBasketPosition( basket, currentChar);
+            cerr << "Ticks [" << ticks << "] allowBackgroundProcessing ["<< allowBackgroundProcessing << "] elapsed [" << elapsed << "] currentChar [" << currentChar << "]" << endl;
+
+            if ( currentChar == LEFT_CHAR || currentChar == RIGHT_CHAR ) 
+            {
+                UpdateBasketPosition( basket, currentChar );
             }
-            
-            if ( fruit.position.row == END_ROW - 1){
-                if (CheckInteraction(fruit, basket)){
-                    ResetFruit(fruit);
-                    UpdateScore(score);
-                    if (elapsedTimePerTick >= 50){
-                        elapsedTimePerTick -= 3;  
+        
+            if ( fruit.position.row == END_ROW - 1 ) 
+            {
+                if ( IsCaught( fruit, basket ) ) // IsCaught called only when row of fruit and basket are equal
+                {
+                    ResetFruit( fruit );
+                    UpdateScore( score );
+                    if ( elapsedTimePerTick >= 50 ) // don't go faster than 50 ms or else too laggy and impossible to win
+                    {
+                        // a known issue is lag, especially as elapsedTimePerTick decreases, because rerendering is occuring too quickly 
+                        elapsedTimePerTick -= 3;
                     }
                 }
-                else{
-                    break;
-                }
-                
+                else
+                {
+                    break; // user unable to catch the fruit
+                }  
             }
-            UpdateFruitPosition(fruit);
+            // update screen every time interval
+            UpdateFruitPosition( fruit );
             ClearScreen();
             HideCursor();
             DrawBoard();
             DrawFruit( fruit );
             DrawBasket( basket );
-            DrawScore(score);
+            DrawScore( score );
 
             // Clear inputs in preparation for the next iteration
             startTimestamp = endTimestamp;    
             cerr << currentChar << endl;
             currentChar = NULL_CHAR;
         }
-        if (currentChar == 'a' || currentChar == 'd') {
-            continue;
+        // Only get keyboard input if "a" or "d" hasn't been registered yet, otherwise getchar will override keyboard input to produce a null character
+        if ( !( currentChar == LEFT_CHAR || currentChar == RIGHT_CHAR ) ) {
+            currentChar = getchar();
         }
-        currentChar = getchar();
-        
-
-        // read( 0, &currentChar, 1 );
-
-        //     UpdateFruitPosition( fruit );
-        //     UpdateBasketPosition( basket, )
-        //     ClearScreen();
-        //     DrawBoard();
-        //     DrawFishies( fishies );
-
-        //     if ( showCommandline )
-        //     {
-        //         cerr << "Showing Command Line" << endl;
-        //         MoveTo( 21, 1 ); 
-        //         ShowCursor();
-        //         cout << "Command:" << flush;
-        //     }
-        //     else { HideCursor(); }
-
-        //     // Clear inputs in preparation for the next iteration
-        //     startTimestamp = endTimestamp;    
-        //     currentChar = NULL_CHAR;
-        // }
-        // // Depending on the blocking mode, either read in one character or a string (character by character)
-        // if ( showCommandline )
-        // {
-        //     while ( read( 0, &currentChar, 1 ) == 1 && ( currentChar != '\n' ) )
-        //     {
-        //         cout << currentChar << flush; // the flush is important since we are in non-echoing mode
-        //         currentCommand += currentChar;
-        //     }
-        //     cerr << "Received command [" << currentCommand << "]" << endl;
-        //     currentChar = NULL_CHAR;
-        // else
-        // {
-        //     read( 0, &currentChar, 1 );
-        // }
     }
-    // Tidy Up and Close Down
-
+    
+    // Screen redrawn to remove score counter and print game over screen
     ClearScreen();
     DrawBoard();
     DrawFruit( fruit );
     DrawBasket( basket );
-    DrawGameOver(score);
+    DrawGameOver( score );
+
+    // Tidy Up and Close Down
     ShowCursor();
     SetNonblockingReadState( false );
     TeardownScreenAndInput();
